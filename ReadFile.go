491984859523697f4f7361c840fb16f2f5661dfd
j33pguy <jeepguygaming@gmail.com>
2022-04-25 11:04:15 -0400

    init commit

---
+++ ReadFile.go
@@ -0,0 +1,12 @@
+package utils
+
+type NewStruct struct {
+	param   string
+	baseurl string
+}
+
+// func (ns NewStruct) GetReport(resp *[]byte) *[]byte {
+// o := new(ns)
+// json.Unmarshal(*resp, o)
+// return &o
+// }
