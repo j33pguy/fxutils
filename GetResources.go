9d6ba01ba748994c4d8b6fc97f96ed31da22fdff
j33pguy <jeepguygaming@gmail.com>
2022-04-25 23:44:28 -0400

    updating

--- GetResources.go
+++ GetResources.go
@@ -6,15 +6,10 @@ import (
 	"log"
 )
 
-// DONE checking to make sure i spelled resources correctly <<<< SMOOTH BRAIN!!!
-
 // TODO recheck all spelling of files after today.....too many booze<did that on purpose>
-
-// call this file ReadResource???
 // TODO think about naming for clarity
 
 // Read from foxholeservices warapi
-// FuncSig[Struct](args/params) Return<struct> {}
 func GetFXApi[T any](p string) *T {
 	b := Baseurl(p)
 	r := Response(*b)
@@ -26,7 +21,6 @@ func GetFXApi[T any](p string) *T {
 }
 
 // Read from local resoruce <json file>
-// FuncSig[Struct](args/params) Return<*[]byte> {}
 func GetLocalJson[T any](p string) *[]byte {
 	// mv data,err to package??? is it repeating???
 	data, err := ioutil.ReadFile(p)
