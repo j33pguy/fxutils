8178d299cd9ba1c686940166954c30ae8ae117c5
j33pguy <jeepguygaming@gmail.com>
2022-04-26 08:48:19 -0400

    working on parser, making sure returning correct types

--- GetResources.go
+++ GetResources.go
@@ -11,31 +11,26 @@ func GetFXApi[T any](p string) *T {
 	b := Baseurl(p)
 	r := Response(*b)
 
-	// parse json
-	o := parseJson[T](r)
-	// TODO Remove bottom 3 comments
-	// o := new(T)
-	// json.Unmarshal(*r, o)
-	//
+	o := parseJSON[T](r)
+
 	return o
 }
 
 // Read from local resoruce <json file>
-func GetLocalJson[T any](p string) *T {
+func GetLocalJSON[T any](p string) *T {
 	// Try to get/read local file, else fail
 	d, err := ioutil.ReadFile(p)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// parse json
-	o := parseJson[T](&d)
+	o := parseJSON[T](&d)
 
 	return o
 }
 
-// local only should return *[]
-func parseJson[T any](b *[]byte) *T {
+// local only....should return *T
+func parseJSON[T any](b *[]byte) *T {
 	o := new(T)
 	json.Unmarshal(*b, o)
 
